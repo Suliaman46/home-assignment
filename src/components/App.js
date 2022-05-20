04233c0edb5a26fd42569736f33c7fb3dc29de5f
Suliaman Abass <suliaman.abass46@gmail.com>
2022-05-20 20:34:06 +0200

    Presenting pulled responses from random word api

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import randomWordsAPI from "../apis/randomWordsAPI";
 import Input from "./Input";
 
 
-
-
 const App = ()=>{
     const [words, setWords] = useState([])
 
@@ -22,9 +20,24 @@ const App = ()=>{
     const handleSubmit = (numWords) =>{
         sendRequest(numWords)
     }
+
+    const renderedWords = words.map((word) =>{
+        return (                    
+        <div className="col-md-auto">
+            <div className="ui segment">
+                {word}
+            </div>
+        </div>
+        );
+    })
+
     return (
         <div className="ui container">
             <Input handleSubmit={handleSubmit}/>
+            <div className="ui medium header">Fetched Random Words:</div>
+                <div class="ui equal width grid">
+                    {renderedWords}
+                </div>
         </div>
     );
 }
