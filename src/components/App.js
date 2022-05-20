26bb3d7797adfc9c83aa19e5dd6f96ceb9aeea2d
Suliaman Abass <suliaman.abass46@gmail.com>
2022-05-20 19:12:58 +0200

    Crude implementation of fetching the random names

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import randomWordsAPI from "../apis/randomWordsAPI";
+import Input from "./Input";
+
+
+
 
 const App = ()=>{
-    return (<div>App</div>)
+    const [words, setWords] = useState([])
+
+    const sendRequest = async (num) =>{
+        const responses = [];
+        for(let i =0; i< num; i++) {
+            let response = await randomWordsAPI.get('word')
+            responses.push(response.data[0].word);
+        }
+        setWords(responses);
+        console.log(responses)
+
+    }
+
+    const handleSubmit = (numWords) =>{
+        sendRequest(numWords)
+    }
+    return (
+        <div className="ui container">
+            <Input handleSubmit={handleSubmit}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+
+const Input = (props) =>{
+    const [numWords, setNumWords] = useState(1);
+
+    const onSubmit =(event)=>{
+        event.preventDefault();
+        props.handleSubmit(numWords);
+    }
+
+    return (
+        <div className="ui segment">
+            <form className="ui form" onSubmit={onSubmit}>
+                <div className="field">
+                    <label>No of Words</label>
+                    <input type={`number`} value ={numWords} onChange={(e) => setNumWords(e.target.value)} min={1} max={6}></input>
+                    <span className="validity"></span>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Input;
